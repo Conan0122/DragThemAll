ef3ec771bdcf3b01f3803c0997dcdee221d1cf31
Conan0122 <109999045+Conan0122@users.noreply.github.com>
2023-03-04 03:20:47 +0530

    Adding Unity Analytics, Fixing few bugs and much more !!
    * Added Unity Analytics for analyzing player's behaviors.
    * Fixed the bug where current user level wasn't shown properly in analytics dashboard.
    * Modified sound sfx mechanism to play Big button sfx using separate method so that we can play it wherever needed.
    * Changed splash screen to OneBug games Logo.
    * Changed the resolution of game and set it for 18:9.
    * Fixed the bug occurred because of changing resolution to 18:9 and not fixing level scene for enemy collision.

--- Assets/Scripts/SceneControls.cs
+++ Assets/Scripts/SceneControls.cs
@@ -6,11 +6,12 @@
         // DON'T MAKE IT SINGLETON CLASS //
 */
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
 
 public class SceneControls : MonoBehaviour
 {
@@ -43,9 +44,12 @@ public class SceneControls : MonoBehaviour
     [SerializeField] Animator sceneTransitionAnim;
     [SerializeField] float sceneTransitionDelay = 1f;
 
+    int userLevel;   // Get this from LevelLoad
     int currentSceneIndex;
     bool isPaused = false;
     bool isPopUpDisplayed = false;
+
+    const string USER_LEVEL = "userLevel";
 
     #endregion
 
@@ -199,7 +203,17 @@ public class SceneControls : MonoBehaviour
     public void Restart()
     {
         sceneTransitionAnim.SetTrigger("Start");
-        StartCoroutine(WaitAndLoadScene_Int(currentSceneIndex, Sounds.AudioName.NormalButtonClicks));
+        // TO be used in analytics purpose
+        // to analyse in which level does player restarts level most.
+        // Set userLevel to whatever saved in playerprefs for current level
+        userLevel = PlayerPrefs.GetInt(USER_LEVEL, 1);
+        Analytics.CustomEvent("restartLevel__", new Dictionary<string, object>
+        {
+            { "userLevel" , userLevel}
+        });
+        Debug.Log($"UserLevel when restarting = " + userLevel);
+
+        StartCoroutine(WaitAndLoadScene<int>(currentSceneIndex, Sounds.AudioName.NormalButtonClicks));
         Time.timeScale = 1;
     }
 
@@ -207,14 +221,24 @@ public class SceneControls : MonoBehaviour
     {
         // Move to Level selector scene
         // Change this to specific scene later
-        StartCoroutine(WaitAndLoadScene_Int(currentSceneIndex + 1, Sounds.AudioName.BigButtonClicks));
+
+        Analytics.CustomEvent("playButton__");
+        // Debug.Log($"Custom event PlayButton " + analyticsResult);
+
+        StartCoroutine(WaitAndLoadScene<int>((currentSceneIndex + 1), Sounds.AudioName.BigButtonClicks));
+    }
+
+    public void PlayBigButtonSFX()
+    {
+        // Script just to play Big button sfx wherever needed
+        AudioManager.instance.PlayAudio(Sounds.AudioName.BigButtonClicks, false);
     }
 
     public void GoToScene(string sceneName)
     {
         DataPersistenceManager.instance.LoadFile();
         //  Can put this on back button well.
-        StartCoroutine(WaitAndLoadScene_String(sceneName, Sounds.AudioName.NormalButtonClicks));
+        StartCoroutine(WaitAndLoadScene<string>(sceneName, Sounds.AudioName.NormalButtonClicks));
         Time.timeScale = 1;
     }
 
@@ -226,51 +250,90 @@ public class SceneControls : MonoBehaviour
                             We will inform players that they've complete all the levels,
                             either they restart game again from level 1 or they can wait for new updates.
         */
-        StartCoroutine(WaitAndLoadScene_Int(currentSceneIndex + 1, Sounds.AudioName.NormalButtonClicks));
+        StartCoroutine(WaitAndLoadScene<int>(currentSceneIndex + 1, Sounds.AudioName.NormalButtonClicks));
         Time.timeScale = 1;
     }
 
     public void LevelLoad(string level)
     {
-        StartCoroutine(WaitAndLoadScene_String("L" + level, Sounds.AudioName.NormalButtonClicks));
+        StartCoroutine(WaitAndLoadScene<string>("L" + level, Sounds.AudioName.NormalButtonClicks));
         DataPersistenceManager.instance.ReturnActiveLevel(level);
+        // Parse the current level(string) to integer
+        // To use later to receive it in Analytics purpose
+        Int32.TryParse(level, out userLevel);
+        // Save userLevel in playerprefs
+        // and then use it to know whats current level
+        PlayerPrefs.SetInt(USER_LEVEL, userLevel);
+        Debug.Log($"UserLevel on load = " + userLevel);
     }
 
     public void BackHomeButton()
     {
-        StartCoroutine(WaitAndLoadScene_String("HomeScene", Sounds.AudioName.NormalButtonClicks));
+        StartCoroutine(WaitAndLoadScene<string>("HomeScene", Sounds.AudioName.NormalButtonClicks));
         Time.timeScale = 1;
     }
 
     public void LevelSelectorButton()
     {
         Time.timeScale = 1;
-        StartCoroutine(WaitAndLoadScene_String("LevelSelectorScene", Sounds.AudioName.NormalButtonClicks));
+        StartCoroutine(WaitAndLoadScene<string>("LevelSelectorScene", Sounds.AudioName.NormalButtonClicks));
     }
 
-    IEnumerator WaitAndLoadScene_Int(int scene, Sounds.AudioName audioName)
+    // For Big button with scene transition 
+    // for first 3 big button in home scene
+    public void Experiment<T>(T scene)
+    {
+        StartCoroutine(WaitAndLoadScene<T>(scene, Sounds.AudioName.NormalButtonClicks));
+        Time.timeScale = 1;
+    }
+
+    IEnumerator WaitAndLoadScene<T>(T scene, Sounds.AudioName audioName)
     {
         if (AudioManager.instance != null)
         {
-            // For scenes based on index
             AudioManager.instance.PlayAudio(audioName, false);
-            sceneTransitionAnim.SetTrigger("Start");
-            yield return new WaitForSeconds(sceneTransitionDelay);
-            SceneManager.LoadScene(scene);
         }
 
-    }
+        sceneTransitionAnim.SetTrigger("Start");
+        yield return new WaitForSeconds(sceneTransitionDelay);
 
-    IEnumerator WaitAndLoadScene_String(string scene, Sounds.AudioName audioName)
-    {
-        if (AudioManager.instance != null)
+        if (typeof(T) == typeof(int))
         {
-            // For levels
-            AudioManager.instance.PlayAudio(audioName, false);
-            sceneTransitionAnim.SetTrigger("Start");
-            yield return new WaitForSeconds(sceneTransitionDelay);
-            SceneManager.LoadScene(scene);
+            SceneManager.LoadScene((int)(object)scene);
+        }
+        else if (typeof(T) == typeof(string))
+        {
+            SceneManager.LoadScene((string)(object)scene);
         }
     }
 
+    
 }
+
+
+//  UNUSED CODE
+
+
+// IEnumerator WaitAndLoadScene_Int(int scene, Sounds.AudioName audioName)
+// {
+//     if (AudioManager.instance != null)
+//     {
+//         // For scenes based on index
+//         AudioManager.instance.PlayAudio(audioName, false);
+//     }
+//     sceneTransitionAnim.SetTrigger("Start");
+//     yield return new WaitForSeconds(sceneTransitionDelay);
+//     SceneManager.LoadScene(scene);
+// }
+
+// IEnumerator WaitAndLoadScene_String(string scene, Sounds.AudioName audioName)
+// {
+//     if (AudioManager.instance != null)
+//     {
+//         // For levels
+//         AudioManager.instance.PlayAudio(audioName, false);
+//     }
+//     sceneTransitionAnim.SetTrigger("Start");
+//     yield return new WaitForSeconds(sceneTransitionDelay);
+//     SceneManager.LoadScene(scene);
+// }
